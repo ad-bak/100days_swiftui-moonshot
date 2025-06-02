c13630bbe35c6b5e55f712e9dc3dce4d1c5ab5bc
adbak <adbak.dev@gmail.com>
2025-06-02 19:39:11 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Adnan Bakaev  on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
